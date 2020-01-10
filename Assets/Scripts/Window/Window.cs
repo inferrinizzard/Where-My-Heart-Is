1d6b209012ab569ebf867291b785c11219869432
YouyouZzz <47422184+YouyouZzz@users.noreply.github.com>
2020-01-10 15:23:46 -0800

    Stubs for cut tool application and world organization

---
+++ Assets/Scripts/Window/Window.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Window : InteractableObject
+{
+    public RealConfig realConfiguration;
+    public DreamConfiguration dreamConfiguration;
+    public GameObject FieldOfView;
+    public override void Interact(PlayerManager player)
+    {
+        if(transform.parent == null)
+        {
+            ApplyCut();
+            transform.parent = player.transform;
+        }
+        else
+        {
+            transform.parent = null;
+        }
+    }
+
+    public void ApplyCut()
+    {
+        // for all real configuration children
+            // cut using CSG.Operations.Union
+                //
+            // object 1: the current child
+            // object 2: Test field
+        foreach(ClipableObject clippableObject in realConfiguration.GetClipableObjects())
+        {
+            clippableObject.UnionWith(FieldOfView);
+        }
+
+        // for all dream configuration children
+            // cut using CSG.Operations.Subtract (object - field)// when available
+            // object 1: the current child
+            // object 2: Test field
+
+
+        // remove all previous cut results
+
+    }
+}
---
+++ Assets/Scripts/World/ClipableObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClipableObject : MonoBehaviour
+{
+    public CSG.Operations operations;
+    public GameObject ClippingResultPrefab;
+
+    public void UnionWith(GameObject other)
+    {
+        GameObject result = GameObject.Instantiate(ClippingResultPrefab);
+        result.transform.position = this.transform.position;
+        result.transform.rotation = this.transform.rotation;
+
+        operations.Union(this.gameObject, other, result);
+    }
+
+    public void Subtract(GameObject other)
+    {
+        //TODO: use subtract when available
+        /*GameObject result = GameObject.Instantiate(ClippingResultPrefab);
+        result.transform.position = this.transform.position;
+        result.transform.rotation = this.transform.rotation;
+
+        operations.Union(this.gameObject, other, result);*/
+    }
+}
