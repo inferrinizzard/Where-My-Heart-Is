dde38a0a5017de7fd5ac37c0952a8aec7a5c4f4f
Sanchit Keni <sskeni@ucsc.edu>
2020-01-18 16:46:24 -0800

    Added comments and started object interaction
    Started commenting locomotion code. Added player picking up methods. Added object pickupable script which handles rotation and position of a picked up object.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,49 +2,88 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Handles player movement and player interaction.
+/// </summary>
 [System.Serializable]
 public class PlayerMovement : MonoBehaviour
 {
+    // Reference to player CharacterController
     private CharacterController characterController;
-    private GameObject lookRoot;
+
+    // Reference to player Camera
+    private Camera cam;
+    private GameObject heldObjectLocation;
+    private GameObject heldObject;
     private Vector3 moveDirection;
     private float verticalVelocity;
     private float playerHeight;
+    private bool playerMovement = true;
+    private bool holding = false;
+    private bool looking = false;
 
-    [SerializeField] private float speed;
-    [SerializeField] private float gravity;
-    [SerializeField] private float jumpForce;
-    [SerializeField] private float mouseSensitivity;
-    [SerializeField] private KeyCode jumpKey;
-    [SerializeField] private KeyCode crouchKey;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float gravity = 25f;
+    [SerializeField] private float jumpForce = 7f;
+    [SerializeField] private float mouseSensitivity = 2f;
+    [SerializeField] private float playerReach = 4f;
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] private KeyCode crouchKey = KeyCode.LeftShift;
+    [SerializeField] private KeyCode pickUpKey = KeyCode.E;
 
     // Camera Variables
-    float minX = -85f;
-    float maxX = 85f;
+    float minX = -90f;
+    float maxX = 90f;
 
     float rotationY = 0f;
     float rotationX = 0f;
 
+    /// <summary>
+    /// Initializes variables before the game starts.
+    /// </summary>
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
-        lookRoot = GameObject.FindGameObjectWithTag("LookRoot");
+        cam = GetComponentInChildren<Camera>();
         playerHeight = characterController.height;
+
+        // Creates an empty game object at the position where a held item should be
+        heldObjectLocation = new GameObject("HeldObjectLocation");
+        heldObjectLocation.transform.parent = cam.transform;
+        heldObjectLocation.transform.position = cam.transform.position + Vector3.forward;
+
     }
 
+    /// <summary>
+    /// Called when the script is enabled.
+    /// </summary>
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        playerMovement = true;
+        holding = false;
+        looking = false;
     }
 
+    /// <summary>
+    /// Called once per frame.
+    /// </summary>
     void Update()
     {
-        Move();
-        Crouch();
-        Rotate();
+        if(playerMovement)
+        {
+            Move();
+            Crouch();
+            Rotate();
+        }
+
+        PickUp();
     }
 
-    // Move Player using Horizontal and Vertical Axes
+    /// <summary>
+    /// Moves and applies gravity to the player using Horizonal and Vertical Axes.
+    /// </summary>
     private void Move()
     {
         moveDirection = Input.GetAxis("Vertical") * transform.forward + Input.GetAxis("Horizontal") * transform.right;
@@ -55,27 +94,35 @@ public class PlayerMovement : MonoBehaviour
         characterController.Move(moveDirection);
     }
 
-    // Apply gravity to the player
+    /// <summary>
+    /// Applies gravity to the player and includes jump.
+    /// </summary>
     void ApplyGravity()
     {
-        verticalVelocity -= gravity * Time.deltaTime;
+        if(!characterController.isGrounded)
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
 
         Jump();
 
         moveDirection.y = verticalVelocity * Time.deltaTime;
     }
 
-    // Player jump function
+    /// <summary>
+    /// Player jump function.
+    /// </summary>
     void Jump()
     {
-        if (characterController.isGrounded && Input.GetKeyDown(jumpKey))
+        if(characterController.isGrounded && Input.GetKeyDown(jumpKey))
         {
             verticalVelocity = jumpForce;
         }
     }
 
-    // Rotate player based on mouse movement
-    // Code edited from Scripts/Player/PlayerManager.cs
+    /// <summary>
+    /// Rotates the player and camera based on mouse movement.
+    /// </summary>
     private void Rotate()
     {
         rotationY += Input.GetAxis("Mouse X") * mouseSensitivity;
@@ -84,16 +131,21 @@ public class PlayerMovement : MonoBehaviour
         rotationX = Mathf.Clamp(rotationX, minX, maxX);
 
         transform.localEulerAngles = new Vector3(0, rotationY, 0);
-        lookRoot.transform.localEulerAngles = new Vector3(-rotationX, 0, 0);
+        cam.transform.localEulerAngles = new Vector3(-rotationX, 0, 0);
 
-        if (Input.GetKey(KeyCode.Escape))
+        if(Input.GetKey(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
     }
 
-    // Player crouch function
+    /// <summary>
+    /// Player crouch function.
+    /// </summary>
+    /// <remarks>
+    /// Also checks to see if the player can uncrouch.
+    /// </remarks>
     private void Crouch()
     {
         Ray crouchRay = new Ray(transform.position, Vector3.up);
@@ -102,9 +154,55 @@ public class PlayerMovement : MonoBehaviour
         {
             characterController.height = playerHeight/2;
         }
-        else if (!Physics.Raycast(crouchRay, out RaycastHit hit, playerHeight * 3/4)) // Check if there is anything above the player
+        else if(!Physics.Raycast(crouchRay, out RaycastHit hit, playerHeight * 3 / 4)) // Check if there is anything above the player
         {
             characterController.height = playerHeight;
         }
     }
+
+    /// <summary>
+    /// Handles player behavior with picking up and inspecting items.
+    /// </summary>
+    private void PickUp()
+    {
+        RaycastHit hit;
+        if(Input.GetKeyDown(pickUpKey))
+        {
+            if(!holding)
+            {
+                if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, playerReach) && hit.transform.tag == "Pickupable")
+                {
+                    //Debug.Log(hit.transform.tag);
+                    //Debug.DrawRay(cam.transform.position, cam.transform.forward * playerReach, Color.red, 2);
+                    heldObject = hit.collider.gameObject;
+                    heldObject.GetComponent<Pickupable>().PickUp(this);
+                    looking = true;
+                    holding = true;
+                    playerMovement = false;
+                }
+            }
+            else if(looking)
+            {
+                heldObject.GetComponent<Pickupable>().StopLooking();
+                looking = false;
+                playerMovement = true;
+            }
+            else
+            {
+                heldObject.GetComponent<Pickupable>().Drop();
+                holding = false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Function to get transform of where the held object should be.
+    /// </summary>
+    /// <returns>
+    /// Returns a reference to the player's heldObjectLocation transform.
+    /// </returns>
+    public Transform GetHeldObjectLocation()
+    {
+        return heldObjectLocation.transform;
+    }    
 }
---
+++ Assets/Scripts/World/Pickupable.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickupable : MonoBehaviour
+{
+    private bool holding = false;
+    private bool looking = false;
+    private Vector3 targetPos;
+    private PlayerMovement player;
+
+    void Update()
+    {
+        if(holding) Holding();
+        if(looking) Looking();
+    }
+
+    public void Holding()
+    {
+        targetPos = player.GetHeldObjectLocation().position;
+        transform.position = targetPos;
+        if(!looking)
+        {
+            // Rotate the object based on the player camera
+            transform.parent = player.GetHeldObjectLocation();
+        }
+    }
+
+    public void Looking()
+    {
+        float rotX = Input.GetAxis("Mouse X") * 2f;
+        float rotY = Input.GetAxis("Mouse Y") * 2f;
+
+        transform.rotation = Quaternion.AngleAxis(-rotX, player.GetHeldObjectLocation().up) * transform.rotation;
+        transform.rotation = Quaternion.AngleAxis(rotY, player.GetHeldObjectLocation().right) * transform.rotation;
+    }
+
+    public void StopLooking()
+    {
+        looking = false;
+    }
+
+    public void PickUp(PlayerMovement player)
+    {
+        this.player = player;
+        looking = true;
+        holding = true;
+    }
+
+    public void Drop()
+    {
+        holding = false;
+        transform.parent = null;
+    }
+}
