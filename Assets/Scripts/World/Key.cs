12c85159939dbacbf53aaa6bba891a6af7f94824
Sanchit Keni <sskeni@ucsc.edu>
2020-01-29 18:40:57 -0800

    Started implementation on key and lock functionality

---
+++ Assets/Scripts/World/Key.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : Pickupable
+{
+
+    public Lock _lock;
+
+    private Transform originPosition;
+
+    void Awake()
+    {
+        originPosition = transform;
+    }
+
+    void Update()
+    {
+        if (active)
+        {
+            // If the object is being held, run Holding.
+            if (player.holding) Holding();
+
+            // If the object is being inspected, run Looking.
+            if (player.looking) Looking();
+        }
+    }
+
+    public override void Interact()
+    {
+        if (!player.holding)
+        {
+            player.holding = true;
+        }
+        else if (player.looking)
+        {
+            player.looking = false;
+        }
+        else
+        {
+            transform.parent = null;
+
+            if(Vector3.Distance(transform.position, _lock.transform.position) < 2 && _lock != null)
+            {
+                this.gameObject.SetActive(false);
+                _lock.Interact();
+            }
+            else
+            {
+                Debug.Log("Oops you dropped this");
+                transform.position = originPosition.position;
+                transform.rotation = originPosition.rotation;
+            }
+            player.holding = false;
+        }
+    }
+}
