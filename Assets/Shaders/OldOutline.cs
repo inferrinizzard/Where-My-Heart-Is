db4b38f68e5ec9172b41e6548c9240c7f402bd8e
inferrinizzard <inferrinizzard@gmail.com>
2020-02-01 19:29:53 -0800

    old outline

---
+++ Assets/Shaders/OldOutline.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class OldOutline : MonoBehaviour
+{
+	[SerializeField] Material postprocessMaterial;
+
+	void Start()
+	{
+		Camera.main.depthTextureMode = Camera.main.depthTextureMode | DepthTextureMode.DepthNormals;
+	}
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		Graphics.Blit(source, destination, postprocessMaterial);
+	}
+}
