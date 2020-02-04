931a350589b9c8694c9c45dfeeef98b42797ba97
JakeBur <44956800+JakeBur@users.noreply.github.com>
2020-02-03 17:06:25 -0800

    Implemented window sound on player

--- Assets/Scripts/Audio/PlayerAudio.cs
+++ Assets/Scripts/Audio/PlayerAudio.cs
@@ -6,6 +6,7 @@ public class PlayerAudio : MonoBehaviour
 {
     public float landingDistanceThreshold;
 
+    [Header("Movement Event References")]
     [FMODUnity.EventRef]
     public string JumpLiftoffEvent;
 
@@ -21,30 +22,53 @@ public class PlayerAudio : MonoBehaviour
     [FMODUnity.EventRef]
     public string WalkEvent;
 
+    [Header("Window Event References")]
+    [FMODUnity.EventRef]
+    public string WindowEvent;
+
     private FMOD.Studio.EventInstance walkInstance;
+    private FMOD.Studio.EventInstance windowInstance;
 
     private void Start()
     {
         walkInstance = FMODUnity.RuntimeManager.CreateInstance(WalkEvent);
         walkInstance.start();
+
+        windowInstance = FMODUnity.RuntimeManager.CreateInstance(WindowEvent);
     }
 
-    public void PlayJumpLiftoff()
+    public void OpenWindow()
+    {
+        windowInstance.setParameterByName("WindowState", 1);
+        windowInstance.start();
+    }
+
+    public void PlaceWindow()
+    {
+        windowInstance.setParameterByName("WindowState", 2);
+    }
+
+    public void CloseWindow()
+    {
+        windowInstance.setParameterByName("WindowState", 0);
+    }
+
+    public void JumpLiftoff()
     {
         FMODUnity.RuntimeManager.PlayOneShot(JumpLiftoffEvent, transform.position);
     }
 
-    public void PlayJumpLanding()
+    public void JumpLanding()
     {
         FMODUnity.RuntimeManager.PlayOneShot(JumpLandingEvent, transform.position);
     }
 
-    public void PlayCrouchDown()
+    public void CrouchDown()
     {
         FMODUnity.RuntimeManager.PlayOneShot(CrouchDownEvent, transform.position);
     }
 
-    public void PlayCrouchUp()
+    public void CrouchUp()
     {
         FMODUnity.RuntimeManager.PlayOneShot(CrouchUpEvent, transform.position);
     }
