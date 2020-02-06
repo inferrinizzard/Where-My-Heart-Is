306914f9c1fab878cef9360f99b0a870dd671c9f
inferrinizzard <inferrinizzard@gmail.com>
2020-02-06 06:19:03 -0800

    update CreateMask

---
+++ Assets/Shaders/Mask/ApplyMask.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ApplyMask : MonoBehaviour
+{
+	[SerializeField] Camera realCam = default;
+	Camera dreamCam;
+
+	[SerializeField] Shader merge = default;
+	Material screenMat;
+	// Start is called before the first frame update
+	void Start()
+	{
+		screenMat = new Material(merge);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
--- Assets/Shaders/Mask/CreateMask.cs
+++ Assets/Shaders/Mask/CreateMask.cs
@@ -11,7 +11,9 @@ public class CreateMask : MonoBehaviour
 		maskCam = GetComponent<Camera>();
 		maskCam.backgroundColor = Color.black;
 
-		var currentSCreen = RenderTexture.active;
+		var currentScreen = RenderTexture.active;
+		// var capture = RenderTexture.GetTemporary(Screen.width, Screen.height, 16, RenderTextureFormat.R8);
+		// maskCam.targetTexture = capture;
 		RenderTexture.active = maskCam.targetTexture;
 		maskCam.Render();
 
@@ -19,10 +21,23 @@ public class CreateMask : MonoBehaviour
 		mask.ReadPixels(new Rect(0, 0, maskCam.targetTexture.width, maskCam.targetTexture.height), 0, 0);
 		mask.Apply();
 
-		RenderTexture.active = currentSCreen;
+		// maskCam.targetTexture = null;
+		// RenderTexture.ReleaseTemporary(capture);
+		RenderTexture.active = currentScreen;
+		maskCam.Render();
 
 		byte[] bytes = mask.EncodeToPNG();
 		System.IO.File.WriteAllBytes("Assets/Resources/Mask.png", bytes);
+		StartCoroutine(WaitFrames());
+
+	}
+
+	IEnumerator WaitFrames(int frames = 2)
+	{
+		int i = 0;
+		while (i++ < frames)
+			yield return null;
 		gameObject.SetActive(false);
+		Destroy(this);
 	}
 }
