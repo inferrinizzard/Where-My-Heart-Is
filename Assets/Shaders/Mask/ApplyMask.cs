6c337266dce8f483528c372d2e097e9e2000d984
inferrinizzard <inferrinizzard@gmail.com>
2020-02-07 02:38:26 -0800

    reset mask, 2 cam 1 RT

--- Assets/Shaders/Mask/ApplyMask.cs
+++ Assets/Shaders/Mask/ApplyMask.cs
@@ -6,23 +6,52 @@ using UnityEngine;
 public class ApplyMask : MonoBehaviour
 {
 	[SerializeField] Camera realCam = default;
-	Camera dreamCam;
+	Camera maskCam;
 	[SerializeField] Shader merge = default;
 	Material screenMat;
 
-	Texture2D mask;
 	RenderTexture real;
+
+	public RenderTexture mask;
 	void Start()
 	{
 		screenMat = new Material(merge);
-		real = new RenderTexture(Screen.width, Screen.height, 16, RenderTextureFormat.Default);
-		realCam.targetTexture = real;
-	}
 
-	public void AssignMask(Texture2D mask)
-	{
-		this.mask = mask;
-		screenMat.SetTexture("_Mask", mask);
+		real = new RenderTexture(Screen.width, Screen.height, 16, RenderTextureFormat.Default);
+		real.name = "Real World";
+		realCam.targetTexture = real;
+
+		// mask = new RenderTexture(Screen.width, Screen.height, 16, RenderTextureFormat.R8);
+		// mask = RenderTexture.GetTemporary(Screen.width, Screen.height, 16, RenderTextureFormat.R8);
+		// mask.Create();
+		// mask.name = "Internal Mask";
+
+		maskCam = new GameObject("Mask Cam").AddComponent<Camera>();
+		maskCam.transform.position = Vector3.zero;
+		maskCam.transform.eulerAngles = Vector3.zero;
+		maskCam.transform.parent = transform;
+		maskCam.transform.localEulerAngles = Vector3.zero;
+		maskCam.transform.localPosition = Vector3.zero;
+
+		maskCam.cullingMask = 1 << LayerMask.NameToLayer("Mask");
+		maskCam.clearFlags = CameraClearFlags.SolidColor;
+		maskCam.backgroundColor = new Color(0, 0, 0, 0);
+		maskCam.targetTexture = mask;
+
+		maskCam.Render();
+
+		var screen = RenderTexture.active;
+		RenderTexture.active = mask;
+
+		var mask2D = new Texture2D(mask.width, mask.height);
+		mask2D.ReadPixels(new Rect(0, 0, mask.width, mask.height), 0, 0);
+		mask2D.Apply();
+		screenMat.SetTexture("_Mask", mask2D);
+
+		RenderTexture.active = screen;
+
+		Destroy(maskCam.gameObject);
+		// RenderTexture.ReleaseTemporary(mask);
 	}
 
 	void OnRenderImage(RenderTexture source, RenderTexture dest)
