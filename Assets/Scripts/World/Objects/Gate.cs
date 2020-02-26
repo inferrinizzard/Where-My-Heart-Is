1455f07fc5c30a70194bc4fe734de401a18f1f98
JakeBur <44956800+JakeBur@users.noreply.github.com>
2020-02-25 21:19:41 -0800

    Partial gate puzzle blockout

---
+++ Assets/Scripts/World/Objects/Gate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    public float rotationAngle;
+    public Transform leftDoor;
+    public Transform rightDoor;
+    public GameObject keyHole;
+
+    private bool open;
+
+    private void Start()
+    {
+        open = false;
+    }
+
+    private void Update()
+    {
+        /*if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Open();
+        }*/
+    }
+
+    public void Open()
+    {
+        if (open) return;
+        open = true;
+
+        leftDoor.Rotate(Vector3.up, rotationAngle);
+        rightDoor.Rotate(Vector3.up, -rotationAngle);
+    }
+}
