ae36e1322706b67b2f77dff2b82d37c521fbb4a2
inferrinizzard <inferrinizzard@gmail.com>
2020-02-26 21:20:48 -0800

    birb test code

---
+++ Assets/Models/Birb/BirbAnimTester.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirbAnimTester : MonoBehaviour
+{
+	public KeyCode idleKey = KeyCode.Alpha1;
+	public KeyCode flyKey = KeyCode.Alpha2;
+	Animator anim;
+	// Start is called before the first frame update
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(flyKey))
+			anim.SetBool("IsFlying", true);
+		if (Input.GetKeyDown(idleKey))
+			anim.SetBool("IsIdle", true);
+	}
+}
