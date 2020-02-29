56a0153b26b39c287ab2211bccf6812b41f8f30d
Sanchit Keni <sskeni@ucsc.edu>
2020-02-29 13:48:12 -0800

    Basic implementation of dialogue system
    Reads from a json and types through text one line at a time until any "/" character, then proceeds to the next line if there is any. Will continue until there are no more lines.

---
+++ Assets/Scripts/Dialogue/DialogueSystem.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour
+{
+    public JsonParsable json;
+    public string path;
+
+    public Text uiText;
+    public string inputText;
+    private string[] textQueue;
+
+    private bool texting = false;
+    private int count = 0;
+    private int activeLineCount = 0;
+    private float textSpeed = 0.1f;
+
+    private void Start()
+    {
+        path = Application.dataPath + "/Scripts/Dialogue/" + path;
+        Debug.Log(path);
+        string jsonString = File.ReadAllText(path);
+        Debug.Log(jsonString);
+        json = JsonUtility.FromJson<JsonParsable>(jsonString);
+        inputText = json.text;
+        ParseText();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) && activeLineCount == 0)
+        {
+            StartCoroutine(WriteDialogue(0));
+        }
+    }
+
+    public void ParseText()
+    {
+        textQueue = inputText.Split('/');
+    }
+
+    public IEnumerator WriteDialogue(float delay)
+    {
+        if(activeLineCount != 0) yield return new WaitForSeconds(delay);
+        if (!texting && activeLineCount < textQueue.Length)
+        {
+            texting = true;
+            InvokeRepeating("AdvanceText", 0f, textSpeed);
+        }
+        else
+        {
+            EndText();
+        }
+    }
+
+    public void AdvanceText()
+    {
+        if (count <= textQueue[activeLineCount].Length && texting)
+        {
+            uiText.text = textQueue[activeLineCount].Substring(0, count);
+            count++;
+        }
+        else
+        {
+            CancelInvoke("AdvanceText");
+            texting = false;
+            count = 0;
+            activeLineCount++;
+            StartCoroutine(WriteDialogue(1));
+        }
+    }
+
+    public void EndText()
+    {
+        uiText.text = "";
+    }
+}
