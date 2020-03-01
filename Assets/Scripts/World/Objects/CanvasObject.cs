a573f9d2278a2d3449eeac206e0b24ce8253c4c2
inferrinizzard <inferrinizzard@gmail.com>
2020-03-01 14:52:43 -0800

    canvas pt2 (redo collectable)

--- Assets/Scripts/World/Objects/CanvasObject.cs
+++ Assets/Scripts/World/Objects/CanvasObject.cs
@@ -2,34 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CanvasObject : InteractableObject
+public class CanvasObject : CollectableObject
 {
-	[SerializeField] float threshold = 1f;
-	bool pickingUp;
-
-	// Start is called before the first frame update
-	protected override void Start()
-	{
-		base.Start();
-		pickingUp = false;
-	}
-
-	private void Update()
-	{
-		if (pickingUp)
-		{
-			transform.position = Vector3.Lerp(transform.position, player.transform.position, 5f * Time.deltaTime);
-
-			if (Vector3.Distance(transform.position, player.transform.position) < threshold)
-			{
-				gameObject.SetActive(false);
-			}
-		}
-	}
+	public Texture2D preview;
+	[SerializeField] string manualTarget = "Bridge";
 
 	public override void Interact()
 	{
-		pickingUp = true;
-		GameManager.Instance.ChangeLevel(GameManager.Instance.levels[GameManager.Instance.sceneIndex + 1]);
+		//prevent move/rotate here
+		StartCoroutine(Collect(
+			player.transform.position + player.cam.transform.forward,
+			new Vector3(player.rotationX - 25f, player.rotationY, 0)));
+	}
+
+	protected override void CollectEndAction()
+	{
+		// pass in on start load
+		// set shader flag
+		// continually set global float in coro
+		if (manualTarget != "")
+			GameManager.Instance.ChangeLevel(manualTarget);
+		else
+			GameManager.Instance.ChangeLevel(GameManager.Instance.levels[GameManager.Instance.sceneIndex + 1]);
 	}
 }
