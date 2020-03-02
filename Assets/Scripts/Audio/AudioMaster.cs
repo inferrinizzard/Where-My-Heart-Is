9155cbd5522dcb61d1ffe29575bdb45749c2ff5b
JakeBur <44956800+JakeBur@users.noreply.github.com>
2020-03-01 21:53:56 -0800

    In progress final level

--- Assets/Scripts/Audio/AudioMaster.cs
+++ Assets/Scripts/Audio/AudioMaster.cs
@@ -5,21 +5,54 @@ using UnityEngine;
 public class AudioMaster : Singleton<AudioMaster>
 {
     [FMODUnity.EventRef]
-    public string AmbientEvent;
+    public string AutumnAmbientEvent;
+
+    [FMODUnity.EventRef]
+    public string WinterAmbientEvent;
+
+    [FMODUnity.EventRef]
+    public string TensionMusicEvent;
+
 
     private FMOD.Studio.EventInstance ambientInstance;
+    private FMOD.Studio.EventInstance musicInstance;
 
     // Start is called before the first frame update
     void Start()
     {
-        ambientInstance = FMODUnity.RuntimeManager.CreateInstance(AmbientEvent);
+        ambientInstance = FMODUnity.RuntimeManager.CreateInstance(AutumnAmbientEvent);
         ambientInstance.setParameterByName("Play Song", 1);
         ambientInstance.start();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartTensionTheme()
     {
-        
+        musicInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        musicInstance = FMODUnity.RuntimeManager.CreateInstance(TensionMusicEvent);
+        musicInstance.start();
+    }
+
+    public void SetMusicVariable(string name, float value)
+    {
+        musicInstance.setParameterByName(name, value);
+    }
+
+    public void SetWinter()
+    {
+        ambientInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        ambientInstance = FMODUnity.RuntimeManager.CreateInstance(WinterAmbientEvent);
+        ambientInstance.start();
+    }
+
+    public void SetSpring()
+    {
+        ambientInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        ambientInstance = FMODUnity.RuntimeManager.CreateInstance(WinterAmbientEvent);
+        ambientInstance.start();
+    }
+
+    public void SetNoSong()
+    {
+        ambientInstance.setParameterByName("Play Song", 0);
     }
 }
---
+++ Assets/Scripts/One Offs/AutumnFinalController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutumnFinalController : MonoBehaviour
+{
+    public PlayerTrigger zone1;
+    public PlayerTrigger zone2;
+    public PlayerTrigger zone3;
+
+    private float songState = 0;
+
+    private bool initialized = false;
+
+    private void Update()
+    {
+        if(!initialized)
+        {
+            AudioMaster.Instance.SetNoSong();
+            Player.Instance.audioController.SetWindowWorld(1);
+            initialized = true;
+
+            AudioMaster.Instance.StartTensionTheme();
+
+            zone1.OnPlayerEnter += ProgressSongTo1;
+            zone2.OnPlayerEnter += ProgressSongTo2;
+            zone3.OnPlayerEnter += ProgressSongTo3;
+        }
+    }
+
+    public void ProgressSongTo1()
+    {
+        if(songState < 1)
+        {
+            songState = 1;
+            AudioMaster.Instance.SetMusicVariable("Autumn Tension State", 1);
+        }
+    }
+
+    public void ProgressSongTo2()
+    {
+        if (songState < 2)
+        {
+            songState = 2;
+            AudioMaster.Instance.SetMusicVariable("Autumn Tension State", 2);
+        }
+    }
+
+    public void ProgressSongTo3()
+    {
+        if (songState < 3)
+        {
+            songState = 3;
+            AudioMaster.Instance.SetMusicVariable("Autumn Tension State", 3);
+        }
+    }
+}
