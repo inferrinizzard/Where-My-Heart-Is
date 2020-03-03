6ad5cd1a59989cb451bc6ecfb1dfe732955e4c2c
Niccolo Abate <lofieloomis@gmail.com>
2020-03-02 21:12:40 -0800

    Glide animation added and implimented
    Glide, To Glide, and From Glide animation added
    Animator update (Added variable "IsGliding" to handle gliding)
    Events added to Fly animation and Glide animation to create procedural gliding (Set rate in BirdAnimTester)

--- Assets/Models/Birb/BirbAnimTester.cs
+++ Assets/Models/Birb/BirbAnimTester.cs
@@ -7,6 +7,8 @@ public class BirbAnimTester : MonoBehaviour
 	public KeyCode idleKey = KeyCode.Alpha1;
 	public KeyCode flyKey = KeyCode.Alpha2;
 	Animator anim;
+
+    public float GlidePercent = 0.4f;
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -27,4 +29,12 @@ public class BirbAnimTester : MonoBehaviour
             anim.SetBool("IsFlying", false);
         }
 	}
+
+    public void _UpdateGlide()
+    {
+        if (Random.Range(0f, 1f) < GlidePercent)
+            anim.SetBool("IsGliding", true);
+        else
+            anim.SetBool("IsGliding", false);
+    }
 }
