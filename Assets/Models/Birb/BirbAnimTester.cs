9ee45ed162bc50a31ec3835bacec1e5ad0333e27
sskeni <50187427+sskeni@users.noreply.github.com>
2020-03-02 20:00:33 -0800

    Implemented spline movement with animations
    very botched

--- Assets/Models/Birb/BirbAnimTester.cs
+++ Assets/Models/Birb/BirbAnimTester.cs
@@ -1,29 +1,57 @@
-﻿using System.Collections;
+﻿using BansheeGz.BGSpline.Components;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BirbAnimTester : MonoBehaviour
 {
-	public KeyCode idleKey = KeyCode.Alpha1;
-	public KeyCode flyKey = KeyCode.Alpha2;
-	Animator anim;
-	// Start is called before the first frame update
+    [SerializeField] private KeyCode idleKey = KeyCode.Alpha1;
+    [SerializeField] private KeyCode flyKey = KeyCode.Alpha2;
+    [SerializeField] private BGCcCursor[] curves;
+
+	private Animator anim;
+
+    private int currCurve;
+
 	void Start()
 	{
 		anim = GetComponent<Animator>();
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
-		if (Input.GetKeyDown(flyKey))
-			anim.SetBool("IsFlying", true);
-		if (Input.GetKeyDown(idleKey))
-			anim.SetBool("IsIdle", true);
+        CheckCurvePoints();
 	}
 
-	public void setFlying()
-	{
-		anim.SetBool("IsFlying", true);
-	}
+    private void CheckCurvePoints()
+    {/*
+        Debug.Log(currCurve);
+        Debug.Log(curves[currCurve].DistanceRatio);*/
+        if(currCurve < curves.Length)
+        {
+            if (curves[currCurve].DistanceRatio > 0)
+            {
+                Debug.Log("called!");
+                anim.SetBool("IsFlying", true);
+            }
+            if (curves[currCurve].DistanceRatio > 0.99f)
+            {
+                anim.SetBool("IsFlying", false);
+                currCurve++;
+                StartCoroutine(NextCurve());
+            }
+        }
+    }
+
+    private IEnumerator NextCurve()
+    {
+        if (currCurve < curves.Length)
+        {
+            float tempSpeed = curves[currCurve].GetComponent<BGCcTrs>().Speed;
+            curves[currCurve].GetComponent<BGCcTrs>().Speed = 0;
+            curves[currCurve].DistanceRatio = 0;
+            yield return new WaitForSeconds(3f);
+            curves[currCurve].GetComponent<BGCcTrs>().Speed = tempSpeed;
+        }
+    }
 }
