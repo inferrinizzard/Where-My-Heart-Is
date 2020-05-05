acda12e937c8de2e5f26a05565cdc88fa29d8c63
JakeBur <44956800+JakeBur@users.noreply.github.com>
2020-05-04 20:18:44 -0700

    Completed rematerialization of levels

---
+++ Assets/Scripts/WebPlayerTemplates/ReplaceMaterial.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[ExecuteInEditMode]
+public class ReplaceMaterial : MonoBehaviour
+{
+    public bool replace;
+    public Material toReplace;
+    public Material newMaterial;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(replace)
+        {
+            replace = false;
+            FindObjectsOfType<Renderer>().ToList().ForEach(renderer =>
+            {
+                if(renderer.sharedMaterial == toReplace)
+                {
+                    renderer.sharedMaterial = newMaterial;
+                }
+            });
+        }
+    }
+}
