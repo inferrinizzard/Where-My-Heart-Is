6c5e513b1c310e756a0f4033ce5ca83453e23adb
inferrinizzard <inferrinizzard@gmail.com>
2020-06-05 12:14:40 -0700

    revamp interactables

--- Assets/Scripts/World/Objects/CanvasObject.cs
+++ Assets/Scripts/World/Objects/CanvasObject.cs
@@ -4,19 +4,40 @@ using System.Collections.Generic;
 
 using UnityEngine;
 
-public class CanvasObject : CollectableObject
+public class CanvasObject : Pickupable
 {
 	public event Action OnInteract;
+	[SerializeField] bool onEasel = true;
+	[SerializeField] Transform placeTarget;
 	public override string prompt { get => "Press E to Enter Canvas"; }
+	// public override string prompt { get => "Press E to Place Canvas"; }
 
 	public override void Interact()
 	{
-		OnInteract?.Invoke();
-		StartCoroutine(Player.Instance.mask.PreTransition());
-		var enclippableParent = transform.GetComponentInParent<EntangledClippable>();
-		if (enclippableParent)
-			foreach (var o in enclippableParent.GetComponentsInChildren<OutlineObject>())
-				o.enabled = false;
-		// TODO: await endofframe
+		if (onEasel)
+		{
+			OnInteract?.Invoke();
+			StartCoroutine(Player.Instance.mask.PreTransition());
+			var enclippableParent = transform.GetComponentInParent<EntangledClippable>();
+			if (enclippableParent)
+				foreach (var o in enclippableParent.GetComponentsInChildren<OutlineObject>())
+					o.enabled = false;
+			// TODO: await endofframe
+		}
+		else
+		{
+			if (active && (transform.position - placeTarget.position).sqrMagnitude < player.playerReach * player.playerReach / 4)
+			{
+				PutDown();
+				transform.parent = placeTarget;
+				transform.localPosition = Vector3.zero;
+				transform.localRotation = Quaternion.identity;
+				transform.localScale = Vector3.one;
+				onEasel = true;
+			}
+			else
+				base.Interact();
+			// PickUp();
+		}
 	}
 }
