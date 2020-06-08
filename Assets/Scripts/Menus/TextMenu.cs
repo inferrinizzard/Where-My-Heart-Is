3b2ba31ef22c8b0888a4667c77cda0d89d25b864
Sanchit <50187427+sskeni@users.noreply.github.com>
2020-06-08 16:28:15 -0700

    Implemented dialogue settings

--- Assets/Scripts/Menus/TextMenu.cs
+++ Assets/Scripts/Menus/TextMenu.cs
@@ -10,9 +10,14 @@ public class TextMenu : MonoBehaviour
     public Slider textSizeSlider;
     /// <summary> Text to display current text size. </summary>
     public Text textSizeText;
+    public Toggle textToggle;
+    public GameObject dialogueObject;
+    DialogueSystem dialogueSystem;
 
     private void Start()
     {
+        dialogueSystem = dialogueObject.GetComponentInChildren<DialogueSystem>(true);
+        Debug.Log(dialogueSystem);
         SetTextSizeSlider(); // Set the text at the beginning.
     }
 
@@ -21,10 +26,25 @@ public class TextMenu : MonoBehaviour
     {
         int input = (int) textSizeSlider.value;
         if (input == 0)
+        {
             textSizeText.text = "small";
+            dialogueSystem.uiText.fontSize = 40;
+        }
         if (input == 1)
+        {
             textSizeText.text = "medium";
+            dialogueSystem.uiText.fontSize = 50;
+        }
         if (input == 2)
+        {
             textSizeText.text = "large";
+            dialogueSystem.uiText.fontSize = 60;
+        }
+    }
+
+    public void SetTextToggle()
+    {
+        if (textToggle.isOn) dialogueObject.SetActive(true);
+        else dialogueObject.SetActive(false);
     }
 }
