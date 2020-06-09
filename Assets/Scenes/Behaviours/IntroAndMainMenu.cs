a9d2e41e9b2c72db9683d5c620750b4f357febc0
inferrinizzard <inferrinizzard@gmail.com>
2020-06-09 10:04:16 -0700

    prevent scene loop

--- Assets/Scenes/Behaviours/IntroAndMainMenu.cs
+++ Assets/Scenes/Behaviours/IntroAndMainMenu.cs
@@ -1,27 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 [CreateAssetMenu(fileName = "IntroAndMainMenu", menuName = "Levels/Behaviours/IntroAndMainMenu")]
 public class IntroAndMainMenu : LevelBehaviour
 {
-    private GameObject mainMenuCamera;
-    private GameObject introCamera;
+	private GameObject mainMenuCamera;
+	private GameObject introCamera;
 
-    public void CameraSetup()
-    {
-        //introCamera = GameObject.Find("Main Camera");
-        //mainMenuCamera = GameObject.Find("MainMenuCamera");
+	public void CameraSetup()
+	{
+		//introCamera = GameObject.Find("Main Camera");
+		//mainMenuCamera = GameObject.Find("MainMenuCamera");
 
-        //introCamera.GetComponent<Camera>().enabled = false;
-        //mainMenuCamera.GetComponent<Camera>().enabled = true;
+		//introCamera.GetComponent<Camera>().enabled = false;
+		//mainMenuCamera.GetComponent<Camera>().enabled = true;
 
-        //GameManager.Instance.pause.MainMenuStart();
-    }
+		//GameManager.Instance.pause.MainMenuStart();
+	}
 
-    public void LoadAdditiveScene()
-    {
-        SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);
-    }
+	public void LoadAdditiveScene()
+	{
+		if (SceneManager.sceneCount == 1)
+			SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);
+	}
 }
