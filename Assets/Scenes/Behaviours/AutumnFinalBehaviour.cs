7cd1f89c1359b14244287cddfe8b7a562367c6ef
inferrinizzard <inferrinizzard@gmail.com>
2020-06-09 20:10:07 -0700

    assign half mesh and position

--- Assets/Scenes/Behaviours/AutumnFinalBehaviour.cs
+++ Assets/Scenes/Behaviours/AutumnFinalBehaviour.cs
@@ -27,11 +27,29 @@ public class AutumnFinalBehaviour : LevelBehaviour
 
 	public void HeartBreak()
 	{
-		Player.Instance.hands.leftHandOn = false;;
+		Player.Instance.hands.leftHandOn = false;
 	}
 
 	public void AssignHeartMesh(Mesh halfHeart)
 	{
 		Player.Instance.heartWindow.GetComponent<MeshFilter>().mesh = halfHeart;
+		Player.Instance.heartWindow.transform.Translate(Vector3.right * .25f);
+
+		// var maskRT = Player.Instance.mask.mask;
+
+		// var half = new Texture2D(maskRT.width, maskRT.height);
+
+		// RenderTexture.active = maskRT;
+		// half.ReadPixels(new Rect(0, 0, maskRT.width, maskRT.height), 0, 0);
+		// for (int i = 0; i < maskRT.width; i++)
+		// 	for (int j = 0; j < maskRT.height; j++)
+		// 		half.SetPixel(i, j, i < maskRT.width / 2 ? Color.black : Color.white);
+		// half.Apply();
+		// RenderTexture.active = null;
+
+		// Graphics.Blit(Player.Instance.mask.mask, maskRT);
+		// Player.Instance.mask.SetMask(maskRT);
+
+		// Player.Instance.mask.CreateMask();
 	}
 }
