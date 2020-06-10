f3e9c2962271666946bfb231e2f5fb5d6c88323b
Sanchit <50187427+sskeni@users.noreply.github.com>
2020-06-10 05:02:07 -0700

    Implemented credits after spring room

--- Assets/Scripts/One Offs/DoorPrompt.cs
+++ Assets/Scripts/One Offs/DoorPrompt.cs
@@ -1,22 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DoorPrompt : InteractableObject
 {
     public GameObject Prompt;
+    public GameObject Credits;
     private bool displayPrompt = false;
+
 
     public override void Interact()
     {
         displayPrompt = !displayPrompt;
         Prompt.SetActive(displayPrompt);
         Cursor.visible = displayPrompt;
- 
-        if(displayPrompt)
+
+        if (displayPrompt)
+        {
             Cursor.lockState = CursorLockMode.None;
+            Player.Instance.canMove = false;
+        }
         else
+        {
             Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 
     /// <summary>
@@ -27,11 +35,15 @@ public class DoorPrompt : InteractableObject
         displayPrompt = !displayPrompt;
         Prompt.SetActive(displayPrompt);
         Cursor.lockState = CursorLockMode.Locked;
+        Player.Instance.canMove = true;
     }
 
     // Load credits scene
     public void StartCredits()
     {
-        StartCoroutine(Player.Instance.mask.PreTransition());
+        Credits.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Player.Instance.canMove = false;
+        GameManager.Instance.pause.gameplayUI.SetActive(false);
     }
 }
